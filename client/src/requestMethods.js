80dd839d85d9229a8706e51a24d02b31d3bf82df
NikhilVikhe7 <nikhikvikhe7@gmail.com>
2022-12-27 19:04:27 +0530

    issue 5 fixed

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 
 const BASE_URL = process.env.REACT_APP_BASE_URL
-// let TOKEN = JSON.parse(localStorage.getItem("persist:root")) && JSON.parse(localStorage.getItem("persist:root")).user ?
-//     JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser ?
-//         JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken : "" : "";
-let TOKEN = '';
+let TOKEN = JSON.parse(localStorage.getItem("persist:root")) && JSON.parse(localStorage.getItem("persist:root")).user ?
+    JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser ?
+        JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken : "" : "";
+// let TOKEN = '';
 
 const updateToken =(token)=>{
     TOKEN = token;
